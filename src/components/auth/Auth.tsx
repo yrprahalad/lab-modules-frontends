7ca078fd7a05a48033a011625622ed482f67866c
rakshi <rakshi.yr@gmail.com>
2023-03-11 17:32:08 +0530

    Added Auth Form

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -7,7 +7,7 @@ import Button from "../../widgets/Button/Button";
 
 
 interface Login {
-    username: string,
+    email: string,
     password: string
 }
 
@@ -20,8 +20,10 @@ interface Register {
 
 const Auth: React.FC = () => {
 
-    const [loginSearchInput, setLoginSerchInputValue] = useState<Login>({ username: '', password: '' })
-    const [registerSearchInput, setRegisterSerchInputValue] = useState<Register>({ username: '', password: '', email: '', role: '' })
+    const [loginSearchInput, setLoginSerchInputValue] = useState<Login>({ email: '', password: '' });
+    const [registerSearchInput, setRegisterSerchInputValue] = useState<Register>({ username: '', password: '', email: '', role: '' });
+
+    const [isLogin, setIslogin] = useState<boolean>(false)
 
     const handleOnChangeForLogin = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
@@ -45,73 +47,102 @@ const Auth: React.FC = () => {
     return <div className={styles.authContainer}>
 
         <div className={styles.brandHolder}>LAB MODULES</div>
-        <div className={styles.inputContainer}>
-            <h4>Register</h4>
-            <span>Please enter your details to process further.</span>
+        {isLogin ?
+            <div className={styles.inputContainer}>
+                <h4>Log In</h4>
+                <span>Please enter your details to process further.</span>
 
-            <div className={styles.inputSegments}>
-                <span>Username</span>
-                <InputText
-                    icon={UserIcon}
-                    value={registerSearchInput.username}
-                    type='text'
-                    name='username'
-                    placeholder='Enter Username'
-                    onChange={e => handleOnChangeForRegister(e)}
-                />
+                <div className={styles.inputSegments}>
+                    <span>Email</span>
+                    <InputText
+                        icon={UserIcon}
+                        value={registerSearchInput.username}
+                        type='text'
+                        name='email'
+                        placeholder='Enter Email'
+                        onChange={e => handleOnChangeForLogin(e)}
+                    />
+                </div>
+
+                <div className={styles.inputSegments}>
+                    <span>Password</span>
+                    <InputText
+                        icon={LockIcon}
+                        value={registerSearchInput.password}
+                        type='password'
+                        name='password'
+                        placeholder='Enter Password'
+                        onChange={e => handleOnChangeForLogin(e)}
+                    />
+
+                </div>
+
+                <Button onClick={handleLoginSubmit} text="LOGIN" type='contrast' />
+
+                <div className={styles.loginText}>
+                    Don't have an account ? <span onClick={() => setIslogin(!isLogin)}>Register.</span>
+                </div>
             </div>
-            <div className={styles.inputSegments}>
-                <span>Email</span>
-                <InputText
-                    icon={UserIcon}
-                    value={registerSearchInput.email}
-                    type='text'
-                    name='email'
-                    placeholder='Enter Email'
-                    onChange={e => handleOnChangeForRegister(e)}
-                />
-            </div>
+            :
+            <div className={styles.inputContainer}>
+                <h4>Register</h4>
+                <span>Please enter your details to process further.</span>
 
-            <div className={styles.inputSegments}>
-                <span>Password</span>
-                <InputText
-                    icon={LockIcon}
-                    value={registerSearchInput.password}
-                    type='password'
-                    name='password'
-                    placeholder='Enter Password'
-                    onChange={e => handleOnChangeForRegister(e)}
-                />
+                <div className={styles.inputSegments}>
+                    <span>Username</span>
+                    <InputText
+                        icon={UserIcon}
+                        value={registerSearchInput.username}
+                        type='text'
+                        name='username'
+                        placeholder='Enter Username'
+                        onChange={e => handleOnChangeForRegister(e)}
+                    />
+                </div>
+                <div className={styles.inputSegments}>
+                    <span>Email</span>
+                    <InputText
+                        icon={UserIcon}
+                        value={registerSearchInput.email}
+                        type='text'
+                        name='email'
+                        placeholder='Enter Email'
+                        onChange={e => handleOnChangeForRegister(e)}
+                    />
+                </div>
 
-            </div>
+                <div className={styles.inputSegments}>
+                    <span>Password</span>
+                    <InputText
+                        icon={LockIcon}
+                        value={registerSearchInput.password}
+                        type='password'
+                        name='password'
+                        placeholder='Enter Password'
+                        onChange={e => handleOnChangeForRegister(e)}
+                    />
 
-            <div className={styles.inputSegments}>
-                <span>Role</span>
-                <InputText
-                    icon={UserIcon}
-                    value={registerSearchInput.role}
-                    type='text'
-                    name='role'
-                    placeholder='Enter Email'
-                    onChange={e => handleOnChangeForRegister(e)}
-                />
-            </div>
+                </div>
 
-            <Button onClick={handleRegisterSubmit} text="REGISTER" type='contrast' />
-            {/* <Button onClick={handleRegisterSubmit} text="LOGIN" type={'primary'} /> */}
+                <div className={styles.inputSegments}>
+                    <span>Role</span>
+                    <InputText
+                        icon={UserIcon}
+                        value={registerSearchInput.role}
+                        type='text'
+                        name='role'
+                        placeholder='Enter Email'
+                        onChange={e => handleOnChangeForRegister(e)}
+                    />
+                </div>
 
-            <div className={styles.loginText}>
-                Already have an account ? <span>Login In.</span>
-            </div>
-        </div>
+                <Button onClick={handleRegisterSubmit} text="REGISTER" type='contrast' />
 
-        {/* <div className={styles.brandHolder} >Lab Module</div>
-        <div className={styles.inputHolder}>
-            <div className={styles.inputSegment}>
-                <InputText icon={UserIcon} value={loginSearchInput.username} type='text' name='username' placeholder='User Name' onChange={e => handleOnChange(e)} />
-                <InputText icon={LockIcon} value={loginSearchInput.password} type='password' name='password' placeholder='Password' onChange={e => handleOnChange(e)} />
-            </div>
-        </div> */}
+                <div className={styles.loginText}>
+                    Already have an account ? <span onClick={() => setIslogin(!isLogin)}>Login In.</span>
+                </div>
+            </div>}
+
     </div>
 };
 
